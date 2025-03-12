10f2c69f502e2e3899cfa5936ab2d589ea91f810
Michael Westwood <michael@honcho.agency>
2025-03-12 18:20:54 +0000

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Critical CSS Generator
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace honchoagency\craftcriticalcssgenerator\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Critical CSS Generator settings
+ */
+class Settings extends Model
+{
+}
